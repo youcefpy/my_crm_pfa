0a5d710ec7a6a696f05051a2990b9b59a3bd5afd
Youcef <youcefboutiche@hotmail.fr>
2025-01-11 15:47:59 +0100

    adding postgresql and styling the form of update lead

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -35,6 +35,11 @@ INSTALLED_APPS = [
     'crm_app',
     "phonenumber_field",
     'dashborad_app',
+
+    "crispy_forms",
+    "crispy_bootstrap5",
+
+
 ]
 
 MIDDLEWARE = [
@@ -72,9 +77,13 @@ WSGI_APPLICATION = 'crm_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.getenv('DB_ENGINE'),
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_ADMIN'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),
+        "PORT": os.getenv('DB_PORT'),
     }
 }
 
@@ -142,3 +151,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
